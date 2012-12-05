3d8081bcdb873a69bd777820b80109d3b5614543
Dawne Flanagan <dbflanag@asu.edu>
2012-12-05 13:25:02 -0700

    trying to get the query image going

--- Code/main.py
+++ Code/main.py
@@ -71,16 +71,45 @@ def get_color_space():
 
 def get_database_name():
     # The default color space
-    default_database = "test"+str(datetime.today().minute)+".db"
+    default_database = "test"+str(datetime.today().minute)+str(datetime.today().second)+".db"
 
     filename = raw_input("Enter a database filename: ").lower()
-    db = filename+str(datetime.today().minute)
+    db = filename+str(datetime.today().minute)+str(datetime.today().second)
     
     if not db:
-        db = default_database
-    
+        db = default_database    
     return db
+
+#Task VII
+def batch_option():
+    response = raw_input("Would you like to perform a batch process?  y/n: ").lower()
+    return response
     
+def process_batch(dir):    
+    image_paths = get_image_files(dir)
+    images = [pil.open(img) for img in image_paths]
+    db = get_database_name()
+    imagedb = imagedata.myDB(db)
+    imagedb.make_db()
+    color_space = get_color_space()
+    print("=========== Batch Processing ===========")
+    print("================ Task I ================")
+    task_I.median_cut_histogram(images, color_space)
+    
+    for image in images:
+        #perform tasks 2 - 6
+        image, image_id = get_image()
+        print("================ Task II ================")        
+        task_II.histogram_generator(image, image_id, color_space, imagedb)
+        print("================ Task III ================")        
+        task_III.dct_freq(image, image_id, color_space, imagedb)
+        print("================ Task IV ================")        
+        task_IV.do_task_4(image, image_id, color_space, imagedb)        
+        print("================ Task V ================")        
+        task_V.do_task_5(image, image_id, color_space, imagedb)        
+        print("================ Task VI ================")        
+        task_VI.dwt_freq(image, image_id, color_space, imagedb) 
+        
 def main(args):
     image_dir = get_image_dir()
     color_space = get_color_space()
@@ -111,6 +140,11 @@ def main(args):
     print("================ Task VI ================")
     image, image_id = get_image()
     task_VI.dwt_freq(image, image_id, color_space, imagedb)
-
+    
+    print("================ Task VI ================")
+    response = batch_option()
+    if response == 'y'
+        batch_dir = get_image_dir()
+        process_batch(batch_dir)
 if __name__ == '__main__':
     main(sys.argv[1:]) # skip first argument ("main.py")
