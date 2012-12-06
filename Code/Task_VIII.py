d22406e89dce920a689b638f49b8615a0b69c012
Dawne Flanagan <dbflanag@asu.edu>
2012-12-05 17:50:43 -0700

    PLEASE REVIEW (more in description)
    I have devised an algorithm i believe will work.  Please review my notes
    inside of Task_VIII.py regarding getting best matches & provide input as
    you see fit.
    
    Will implement this evening.  While I'm in class (starting at 6), I
    intend to test the speed of the processing after updating the query
    functions.

--- Code/Task_VIII.py
+++ Code/Task_VIII.py
@@ -40,7 +40,7 @@ def query_region():
     # this gets our <0,0> for the region
     # row :  if x coord > # of cells in (row - 2), then x(an index #)= row.length -3 (account for index = 0)
     
-    if coord % new_width >= (new_width -2):
+    if coord % new_width >= (new_width - 2):
         x = new_width - 3
     else:
         x = coord % new_width
@@ -52,9 +52,31 @@ def query_region():
     
     for y in range (3):
         for x in range (3):
-            qr.append(coord + x + (new_width * y))
+            qr.append(coord + x + (new_width * y))  #now we have a list of 3 x 3 coord_ids
             
     #using general historgram spec from task one
     histospec = get_histogram_spec()
+    
+    #need to get features of SELECTED COORDINATE CELLS from the lists created above.
+    #each list above ids the coordinate by the index of the list item
+    #simply need to referecne the coordinate id to get from ea list above
+    cellinfo = []
+    for index, coord in qr:
+        cellinfoname = str(coord)  #uniquely name the dictionary of features
+        cellinfoname = {'histogram': ihist[coord], 'dct': idct[coord], 'angle': iangle[coord], 'amplitude': iamp[coord], 'dwt': idwt[coord]}
+        cellinfo[index] = cellinfoname  #should place a dictionary in each index of cellinfo
+    
+    #using Euclidean distance, need to identify "10 best image regions" for each feature
+    #   For each feature item in dictionary, iterate through corresponding database table, gathering
+    #   the values that EQUAL the query's value.  If more than 10 hits, keep going until no more matches, or #   list contains 50 values. If less than 10, find all instances that are +/- 1 up to 50 values.  List #   these cell_ids.  
+    #   
+    #   At the end of this, there will be one list containing up to 50 cell_ids per feature (5 lists)
+    #
+    #   Gather all cell_ids which are in all 5 lists.
+    #   If less than 10 cell_ids, find all that are in 4 lists.
+    #   Use Euclidean distance to determine best matches.  (using actual values of given feature from chosen #   cells)
+    #   Smallest values rank higher
+    #   Treat the features as a 5-tuple vector:  <histogram, dct, angle, amplitude, dwt>  (think:  linear alg)
+    #display the matches 
     
     
